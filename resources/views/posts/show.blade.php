5a091690558faf894483f05a16cc018f7187d07a
Dustin Walker <dwtimp@gmail.com>
2017-08-11 20:52:51 -0500

    setting up post/comment relationships

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,5 +5,15 @@
         <h1>{{ $post->title }}</h1>
 
         {{ $post->body }}
+    <hr>
+        <div class="comments">
+        <ul class="list-group">
+             @foreach ($post->comments as $comments)
+                <li class="list-group-item">{{ comment->body }}</li>
+                <strong>
+                    {{ $comment->created_at->diffForHumans () }}:
+                </strong>
+        </ul>
+        </div>
     </div>
 @endsection
