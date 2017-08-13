cbd8ebd6335149f0e89406fcdc1c5d235ed10aac
Dustin Walker <dwtimp@gmail.com>
2017-08-13 14:34:32 -0500

    added comments section

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -10,6 +10,10 @@ class CommentsController extends Controller
 {
     //store method
     public function store(Post $post) {
-        
+        $this->validate(request(), ['body' => 'required|min:2']);
+        $post->addComment(request('body'));
+
+        return back();
     }
+
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,6 +21,7 @@
             <div class="card">
                 <div class="card-block">
                   <form method="POST" action="/posts/{{ $post->id }}/comments">
+                    {{ csrf_field }}
                       <div class="form-group">
                         <textarea name="body" placeholder="Your comment here" class="form-control" rows="" cols="">
                         </textarea>
